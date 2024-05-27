67c8ab7d471c1058c3983abcc156623c40be3804
nastianuk <nikulina2003nastyal@gmail.com>
2024-05-27 22:11:32 +0300

    module6-task1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+// const navList = document.querySelector('.main-nav__list');
+// const navToggle = document.querySelector('.main-nav__toggle');
+// const clsToggle = document.querySelector('.main-nav__toggle-close');
+
+// navToggle.onclick = function() {
+//   navToggle.classList.add('.main-nav__toggle--hide');
+//   clsToggle.classList.remove('.main-nav__toggle--close');
+//   navList.classList.remove('.main-nav__list--hide');
+// };
+
+// clsToggle.onclick = function() {
+//   navToggle.classList.remove('.main-nav__toggle--hide');
+//   clsToggle.classList.add('.main-nav__toggle--close');
+//   navList.classList.add('.main-nav__list--hide');
+// };
+
