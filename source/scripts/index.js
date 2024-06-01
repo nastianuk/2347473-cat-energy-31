a43f71250d45a0fb3dbed6a6da388ad1d3322ec3
nastianuk <158146982+nastianuk@users.noreply.github.com>
2024-06-01 12:09:46 +0300

    Module6-task1 (#9)
    * module6-task1
    
    * moduule6-task1
    
    * module6-task1
    
    * module6-task1
    
    * добавляет js
    
    * Исправляет css

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+const brgBtn = document.querySelector ('.main-nav__toggle');
+const clsBtn = document.querySelector ('.main-nav__toggle-close');
+const navList = document.querySelector ('.main-nav__list');
+
+brgBtn.onclick = function () {
+  brgBtn.classList.add ('main-nav__toggle--opened');
+  clsBtn.classList.remove ('main-nav__toggle-close--closed');
+  navList.classList.remove ('main-nav__list--nojs');
+};
+
+clsBtn.onclick = function () {
+  clsBtn.classList.add ('main-nav__toggle-close--closed');
+  brgBtn.classList.remove ('main-nav__toggle--opened');
+  navList.classList.add ('main-nav__list--nojs');
+};
